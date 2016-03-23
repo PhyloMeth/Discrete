1eab8a99cc1598a1d2a14d3f49f81d2618db0e8e
Tyson Paulson <TPaulson@vols.utk.edu>
2016-03-23 11:16:06 -0400

    Finished Discrete

--- DiscreteBatch.R
+++ DiscreteBatch.R
@@ -1,42 +1,72 @@
 #You can use code you wrote for the correlation exercise here.
+setwd("~/Desktop/PhyloMeth/DiscreteTraits")
+
 source("DiscreteFunctions.R")
-tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
 
-cleaned.discrete <- CleanData(tree, discrete.data)
+tree <- read.tree("ZanneEtAlChronogram1.tre")
 
-VisualizeData(tree, cleaned.discrete)
+dat<-c("Daucus_carota" , "Conyza_canadensis" , "Setaria_viridis" , "Andropogon_gerardii" , "Pueraria_montana" , "Ambrosia_trifida" , "Datura_stramonium" , "Smallanthus_uvedalia" , "Perilla_frutescens" , "Verbesina_jacksonii" , "Vernonia_gigantea" , "Lespedeza_cuneata" , "Ambrosia_artemisiifolia" , "Cichorium_intybus" , "Albizia_julibrissin" , "Cercis_canadensis" , "Sorghum_halepense" , "Sorghastrum_nutans" , "Ligustrum_sinense" , "Gleditsia_triacanthos" , "Microstegium_vimineum" , "Cyperus_strigosus" , "Mosla_chinensis" , "Symphyotrichum_pilosum" , "Erechtites_hieraciifolius" , "Lespedeza_bicolor" , "Helenium_autumnale" , "Trifolium_pratense" , "Acer_saccharum" , "Lonicera_maackii" , "Liriodendron_tulipifera" , "Ailanthus_altissima")
+
+#discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+
+invasive<-c(1,0,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1)
+woody<-c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,0,1,1,1,1)
+
+#discrete.data<-cbind.data.frame(invasive,woody)
+#row.names(discrete.data)<-dat
+
+names(invasive)<-dat
+names(woody)<-dat
+
+cleaned.discrete.I <- CleanData(tree, invasive)
+cleaned.discrete.W <- CleanData(tree, woody)
+
+VisualizeData(cleaned.discrete.I$phy, invasive)
+VisualizeData(cleaned.discrete.W$phy, woody)
 
 #First, let's use parsimony to look at ancestral states
-cleaned.discrete.phyDat <- phyDat(cleaned.discrete, type="______________") #phyDat is a data format used by phangorn
-anc.p <- ancestral.pars(tree, cleaned.discrete.phyDat)
-plotAnc(tree, anc.p, 1)
+invasive.phyDat <- phyDat(invasive, type="USER", levels=c(0,1)) #phyDat is a data format used by phangorn
+anc.pI <- ancestral.pars(cleaned.discrete.I$phy, invasive.phyDat)
+plotAnc(cleaned.discrete.I$phy, anc.pI, 1)
+
+woody.phyDat <- phyDat(woody, type="USER", levels=c(0,1)) #phyDat is a data format used by phangorn
+anc.pW <- ancestral.pars(cleaned.discrete.W$phy, woody.phyDat)
+plotAnc(cleaned.discrete.W$phy, anc.pW, 1)
 
 #Do you see any uncertainty? What does that meean for parsimony?
 
 #now plot the likelihood reconstruction
-anc.ml <- ancestral.pml(pml(tree, cleaned.discrete.phyDat), type="ml")
-plotAnc(tree, anc.ml, 1)
+anc.ml.I <- ancestral.pml(pml(cleaned.discrete.I$phy, invasive.phyDat), type="ml")
+plotAnc(cleaned.discrete.I$phy, anc.ml.I, 1)
 
+anc.ml.W <- ancestral.pml(pml(cleaned.discrete.W$phy, woody.phyDat), type="ml")
+plotAnc(cleaned.discrete.W$phy, anc.ml.W, 1)
 #How does this differ from parsimony? 
 #Why does it differ from parsimony?
 #What does uncertainty mean?
 
 #How many changes are there in your trait under parsimony? 
-parsimony.score <- ____some_function_____(tree, cleanded.discrete.phyDat)
-print(parsimony.score)
+parsimony.score.I <- parsimony(cleaned.discrete.I$phy, invasive.phyDat)
+print(parsimony.score.I)
 
+parsimony.score.W <- parsimony(cleaned.discrete.W$phy, woody.phyDat)
+print(parsimony.score.W)
 #Can you estimate the number of changes under a likelihood-based model? 
 
 #Well, we could look at branches where the reconstructed state changed from one end to the other. But that's not really a great approach: at best, it will underestimate the number of changes (we could have a change on a branch, then a change back, for example). A better approach is to use stochastic character mapping.
 
-estimated.histories <- make.simmap(tree, cleaned.discrete, model="ARD", nsim=5)
+estimated.histories.I <- make.simmap(cleaned.discrete.I$phy, invasive, model="ARD", nsim=5)
+estimated.histories.W <- make.simmap(cleaned.discrete.W$phy, woody, model="ARD", nsim=5)
 
 #always look to see if it seems reasonable
-plotSimmap(estimated.histories)
+plotSimmap(estimated.histories.I)
+plotSimmap(estimated.histories.W)
 
-counts <- countSimmap(estimated.histories)
-print(counts)
+counts.I <- countSimmap(estimated.histories.I)
+print(counts.I)
+
+counts.W <- countSimmap(estimated.histories.W)
+print(counts.W)
 
 #Depending on your biological question, investigate additional approaches:
 #  As in the correlation week, where hypotheses were examined by constraining rate matrices, one can constrain rates to examine hypotheses. corHMM, ape, and other packages have ways to address this.
--- DiscreteFunctions.R
+++ DiscreteFunctions.R
@@ -6,11 +6,17 @@ library(phangorn)
 
 #You can use code you wrote for the correlation exercise here.
 
+#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
 
-VisualizeData <- function(phy, data) {
-	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
+
+VisualizeData <- function(phy, data) {  
+	print(phy)
+	plot(phy)
+	print(data)
+	names(data)
+	str(data)
 }
 
-CleanData <- function(phy, data) {
-	#treedata() in Geiger is probably my favorite function in R.
+
+CleanData <- function(phy, data) { treedata(phy,data,sort=TRUE)
 }
